75a7086c9ef6d206cb7b8bbb7be47bfc08df9740
Diego Lusberg <diegolusberg97@gmail.com>
2018-11-17 16:59:50 -0300

    Comit Inicial

---
+++ app/src/main/java/py/com/prestosoftware/facepet/FacePetApplication.java
@@ -0,0 +1,13 @@
+package py.com.prestosoftware.facepet;
+
+import android.app.Application;
+
+public class FacePetApplication extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+
+
+    }
+}
